af1184b5ef5f7ce0b3721644ea810469f7031b5e
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-09-02 18:32:43 +0530

    Create EmailValidator.kt

---
+++ kotlin-validate-email-regex/src/main/java/com/jeejava/kotlin/validate/email/regex/EmailValidator.kt
@@ -0,0 +1,25 @@
+package com.jeejava.kotlin.validate.email.regex
+
+class EmailValidator {
+	companion object {
+		@JvmStatic
+		val EMAIL_REGEX = "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})";
+
+		fun isEmailValid(email: String): Boolean {
+			return EMAIL_REGEX.toRegex().matches(email);
+		}
+	}
+}
+
+fun main(args: Array<String>) {
+	println(EmailValidator.isEmailValid("Julia.abc@"));
+	println(EmailValidator.isEmailValid("Samantha@com"));
+	println(EmailValidator.isEmailValid("Samantha_21."));
+	println(EmailValidator.isEmailValid(".1Samantha"));
+	println(EmailValidator.isEmailValid("Samantha@10_2A"));
+	println(EmailValidator.isEmailValid("JuliaZ007"));
+	println(EmailValidator.isEmailValid("Julia@007.com"));
+	println(EmailValidator.isEmailValid("_Julia007.com"));
+	println(EmailValidator.isEmailValid("_Julia007@abc.co.in"));
+	println(EmailValidator.isEmailValid("Julia.007@abc.com"));
+}
